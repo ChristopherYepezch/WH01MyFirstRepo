1ec955c47111e0950262b1302e0a1e7ba154f993
Christopher <yepecri123@gmail.com>
2022-05-08 21:11:40 -0500

    react proyect

---
+++ Proyecto PERN/server/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './pages/Home';
+import React from 'react';
+
+import RouterPage from './pages/RouterPage';
+
+function App() {
+  return (
+<RouterPage/>
+
+  );
+} 
+export default App;
